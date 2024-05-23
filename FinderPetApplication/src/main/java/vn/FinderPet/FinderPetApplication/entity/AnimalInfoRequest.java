7e058a50ac6005483924a5ee4ecc7bd1f7c3c0ed
Zeloxpop <daoducdung2000@gmail.com>
2024-05-23 16:34:31 +0700

    hoàn thành chức năng tìm kiếm nâng cao ở mứt back-end

--- FinderPetApplication/src/main/java/vn/FinderPet/FinderPetApplication/entity/AnimalInfoRequest.java
+++ FinderPetApplication/src/main/java/vn/FinderPet/FinderPetApplication/entity/AnimalInfoRequest.java
@@ -1,20 +1,26 @@
 package vn.FinderPet.FinderPetApplication.entity;
 
+import java.util.List;
+
 public class AnimalInfoRequest {
     private String location;
     private String breed;
     private String name;
     private String size;
-    private String[] age;
-    private String gender;
+    private List<String> age;
+    private List<Boolean> gender;
+    private int pageNumber;
+    private int sizePage;
 
-    public AnimalInfoRequest(String location, String breed, String name, String size, String[] age, String gender) {
+    public AnimalInfoRequest(String location, String breed, String name, String size, List<String> age, List<Boolean> gender, int pageNumber, int sizePage) {
         this.location = location;
         this.breed = breed;
         this.name = name;
         this.size = size;
         this.age = age;
         this.gender = gender;
+        this.pageNumber = pageNumber;
+        this.sizePage = sizePage;
     }
 
     public AnimalInfoRequest() {
@@ -53,29 +59,45 @@ public class AnimalInfoRequest {
         this.size = size;
     }
 
-    public String[] getAge() {
+    public List<String> getAge() {
         return age;
     }
 
-    public void setAge(String[] age) {
+    public void setAge(List<String> age) {
         this.age = age;
     }
 
-    public String getGender() {
+    public List<Boolean> getGender() {
         return gender;
     }
 
-    public void setGender(String gender) {
+    public void setGender(List<Boolean> gender) {
         this.gender = gender;
     }
 
     public String getListAge(){
         String s = "[";
-        for (int i = 0; i < this.getAge().length; i++){
-            s += this.getAge()[i] + ", ";
+        for (int i = 0; i < this.getAge().size(); i++){
+            s += this.getAge().get(i) + ", ";
         }
         s = s.trim().substring(0, s.length() - 2) + "]";
         return s;
+    }
+
+    public int getPageNumber() {
+        return pageNumber;
+    }
+
+    public void setPageNumber(int pageNumber) {
+        this.pageNumber = pageNumber;
+    }
+
+    public int getSizePage() {
+        return sizePage;
+    }
+
+    public void setSizePage(int sizePage) {
+        this.sizePage = sizePage;
     }
 
     @Override
--- FinderPetApplication/src/main/java/vn/FinderPet/FinderPetApplication/service/AnimalsService.java
+++ FinderPetApplication/src/main/java/vn/FinderPet/FinderPetApplication/service/AnimalsService.java
@@ -1,8 +1,30 @@
 package vn.FinderPet.FinderPetApplication.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import vn.FinderPet.FinderPetApplication.dao.AnimalsDAO;
+import vn.FinderPet.FinderPetApplication.entity.Animals;
+
+import java.util.List;
 
 @Service
 public class AnimalsService {
+    private AnimalsDAO animalsDAO;
 
+    @Autowired
+    public AnimalsService(AnimalsDAO animalsDAO) {
+        this.animalsDAO = animalsDAO;
+    }
+
+    public Animals findById(Long id){
+        return this.animalsDAO.findById(id).get();
+    }
+
+    public Page<Animals> searchAnimals(String breedName, String shelterAddress, List<String> ages, List<Boolean> genders, String size, String name, int page, int sizePage) {
+        Pageable pageable = PageRequest.of(page, sizePage);
+        return animalsDAO.findByBreedNameContains(breedName, shelterAddress, ages, genders, size, name, pageable);
+    }
 }
